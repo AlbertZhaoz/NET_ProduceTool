c53ec94e8a9b214d45d9a3680f404b1af7a51cb4
v-honzhao <v-honzhao@microsoft.com>
2021-08-24 15:29:06 +0800

    Init solution

---
+++ ProduceTools/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProduceTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
