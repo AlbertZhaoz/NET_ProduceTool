9169b23ec24559ab9d36c31a3adeffb575d486ff
albertzhao <szdxzhy@outlook.com>
2021-09-24 16:00:43 +0800

    Test

--- ProduceTools/Extensions/GitExtension.cs
+++ ProduceTools/Extensions/GitExtension.cs
@@ -122,7 +122,9 @@ namespace Albert.Extensions
 
                 process.BeginOutputReadLine();
                 process.BeginErrorReadLine();
-                process.StandardInput.WriteLine(command + "&exit");
+                process.StandardInput.WriteLine(command);
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
                 //process.StandardInput.WriteLine("exit");
                 process.WaitForExit();
                 process.Close();
