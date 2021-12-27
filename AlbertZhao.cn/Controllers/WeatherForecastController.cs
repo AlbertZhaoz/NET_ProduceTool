59c9acfe1f496a5b27d1ec1ba70157b6ad5a4e28
albertzhao <szdxzhy@outlook.com>
2021-12-27 18:00:34 +0800

    webapi

--- AlbertZhao.cn/Controllers/WeatherForecastController.cs
+++ AlbertZhao.cn/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace AlbertZhao.cn.Controllers
 {
@@ -18,6 +19,22 @@ namespace AlbertZhao.cn.Controllers
             _logger = logger;
         }
 
+
+        [HttpPost]
+        public ActionResult<LoginResponse> LoginByUserName(LoginRequest request)
+        {
+            if (request.UserName == "admin" && request.Password == "123")
+            {
+                var items = Process.GetProcesses().Select(p => new ProcessInfo(p.Id, p.ProcessName, p.WorkingSet64));
+                return new LoginResponse(true, items.ToArray());
+            }
+            else
+            {
+                return new LoginResponse(false, null);
+            }
+        }
+
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -30,10 +47,72 @@ namespace AlbertZhao.cn.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "GetStudent")]
+        [HttpGet]
         public ActionResult<string> PrintStudent(int id)
         {
-            return "AlbertZhao";
+            if(id == 1)
+            {
+                var person = new Person("Albert", "Zhao");
+                return person.PrintFullNameById(1);
+            }
+            else
+            {
+                return NotFound("人员不存在");
+            }
+        }
+
+        //这样传递的好处更加符合Restful风格
+        [HttpGet("{classNum}/{id}")]
+        public ActionResult<string> PrintInfoByClassNumAndId(int classNum,int id)
+        {
+            return new Person("albert", "zhao").PrintInfo(classNum,id);
+        }
+
+        //这样传递的好处更加符合Restful风格
+        [HttpGet("{schoolNum}/{id}")]
+        //如果捕捉的值和函数参数名字不一致，可以用[FromRoute(Name="名字")]来指定
+        public ActionResult<string> PrintInfoBySchoolNoAndId([FromRoute(Name = "schoolNum")]int schoolNo, int id)
+        {
+            return new Person("albert", "zhao").PrintInfo(schoolNo, id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> AddPerson(Person person)
+        {
+            try
+            {
+                await System.IO.File.WriteAllTextAsync(@"F:\Test\test.txt", person.ToString());
+                return "Add person successfully!";
+            }
+            catch (Exception)
+            {
+                return NotFound($"Failed!{person.ToString()}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public string UpdatePerson([FromRoute(Name ="id")]int id,Person p,[FromHeader(Name ="User-Agent")] string us)
+        {
+            return $"更新成功:{us}";
         }
     }
+
+    public record Person(string firstName, string lastName)
+    {
+        public string PrintFullNameById(int id)
+        {
+            return firstName + " " + lastName; 
+        }
+
+        public string PrintInfo(int classNum,int id)
+        {
+            return $"学生姓名为{firstName+lastName}\n" +
+                $"学生班级为:{classNum}\n" +
+                $"学生id为{id}\n";
+        }
+    }
+
+    public record LoginRequest(string UserName,string Password);
+    public record ProcessInfo(long ID,string Name,long WorkingSet);
+    public record LoginResponse(bool OK, ProcessInfo[] ProcessInfos);
 }
--- AlbertZhao.cn/Program.cs
+++ AlbertZhao.cn/Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var urls = new[] { "http://localhost:3000" };
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(builder =>
+        builder.WithOrigins(urls).AllowAnyMethod().AllowAnyHeader().AllowCredentials()));
 
 var app = builder.Build();
 
@@ -15,6 +18,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//启用中间件 CORS策略
+app.UseCors();
 
 app.UseHttpsRedirection();
 
